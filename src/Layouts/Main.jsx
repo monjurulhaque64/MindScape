dda1d90b91ffc4958d43e2552383819684918992
monjurulhaque64 <monjurulhoque64@gmail.com>
2023-06-10 02:58:29 +0600

    Footer done

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../Shared/Navbar/Navbar';
+import Footer from '../Shared/Footer/Footer';
 
 const Main = () => {
     return (
         <div className='container mx-auto'>
             <Navbar></Navbar>
+            <Footer></Footer>
         </div>
     );
 };
