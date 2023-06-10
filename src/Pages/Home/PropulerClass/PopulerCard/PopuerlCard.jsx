390beaaeb61c443fb0b2cdce50255b52901f14eb
monjurulhaque64 <monjurulhoque64@gmail.com>
2023-06-10 16:49:37 +0600

    card

---
+++ src/Pages/Home/PropulerClass/PopulerCard/PopuerlCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const PopuerlCard = ({ classData }) => {
+  const { name } = classData;
+  return (
+    <div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            {name}
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PopuerlCard;
--- src/Pages/Home/PropulerClass/PropulerClass.jsx
+++ src/Pages/Home/PropulerClass/PropulerClass.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../Compo/SectionTitle/SctionTitle';
+import PopuerlCard from './PopulerCard/PopuerlCard';
 
-const PropulerClass = () => {
+const PopularClass = () => {
+    const [popularClasses, setPopularClasses] = useState([]);
+
+    useEffect(() => {
+        fetch('Class.json')
+            .then((res) => res.json())
+            .then((data) => {
+                setPopularClasses(data);
+            });
+    }, []);
+    const popular = popularClasses
+        .sort((a, b) => b.availableSeats - a.availableSeats)
+        .slice(0, 6);
+
+
+    console.log(popular)
+
     return (
         <div>
-            <SectionTitle heading={"popular class"} subHeading={"Believe you can and you're halfway there."}></SectionTitle>
+            <SectionTitle heading="Popular Classes" subHeading="Believe you can and you're halfway there." />
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6 my-10'>
+                {popular.map((classItem) => (
+          <PopuerlCard key={classItem.id} classData={classItem} />
+        ))}
+            </div>
         </div>
     );
 };
 
-export default PropulerClass;
+export default PopularClass;
