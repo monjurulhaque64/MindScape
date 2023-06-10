f46f9d8d2a80e02405bb786ad00370214e0525fb
monjurulhaque64 <monjurulhoque64@gmail.com>
2023-06-10 14:49:32 +0600

    banner done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -9,28 +9,28 @@ import './Banner.css';
 const Banner = () => {
   return (
     <Carousel>
-      <div className="banner-slide">
+      <div className="banner-slide relative">
         <img src={banner1} className="banner-image" alt="Banner 1" />
-        <div className="banner-content">
-          <h2>Slide 1 Title</h2>
-          <p>Slide 1 description goes here.</p>
-          <button className="banner-button">Button 1</button>
+        <div className="banner-content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-black">
+          <h2 className="text-3xl title font-bold mb-4 text-black">MindScape Summer Camp: Explore, Discover, Create!</h2>
+          <p className="text-lg hidden-on-mobile mb-6">Unleash your child's creativity and curiosity at MindScape Summer Camp - where unforgettable adventures await!</p>
+          <button className="btn btn-info">See All Classes</button>
         </div>
       </div>
-      <div className="banner-slide">
+      <div className="banner-slide relative">
         <img src={banner2} className="banner-image" alt="Banner 2" />
-        <div className="banner-content">
-          <h2>Slide 2 Title</h2>
-          <p>Slide 2 description goes here.</p>
-          <button className="banner-button">Button 2</button>
+        <div className="banner-content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+          <h2 className="text-3xl title font-bold mb-4">Unleash Your Imagination at MindScape Summer Camp</h2>
+          <p className="text-lg hidden-on-mobile mb-6">Join us for an enriching summer experience at MindScape Summer Camp - where learning and fun collide!</p>
+          <button className="btn btn-info">See All Classes</button>
         </div>
       </div>
-      <div className="banner-slide">
+      <div className="banner-slide relative">
         <img src={banner3} className="banner-image" alt="Banner 3" />
-        <div className="banner-content">
-          <h2>Slide 3 Title</h2>
-          <p>Slide 3 description goes here.</p>
-          <button className="banner-button">Button 3</button>
+        <div className="banner-content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+          <h2 className="text-3xl title font-bold mb-4">MindScape Summer Adventures: Ignite Your Curiosity</h2>
+          <p className="text-lg hidden-on-mobile  mb-6">Discover a world of excitement and friendship at MindScape Summer Camp - the perfect summer destination!</p>
+          <button className="btn btn-info">See All Classes</button>
         </div>
       </div>
     </Carousel>
