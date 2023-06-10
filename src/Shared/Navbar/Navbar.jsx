77f28e171e8065acbb95e307c7ea23ee25f9ae5d
monjurulhaque64 <monjurulhoque64@gmail.com>
2023-06-10 14:54:37 +0600

    navbar fixed

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         
     </>
     return (
-        <div className="navbar bg-base-100 mt-4">
+        <div className="navbar fixed z-10 bg-opacity-40 container bg-black text-white">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
