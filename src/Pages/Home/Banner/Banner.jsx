67205b3c33971e9499fb476c913d0d2d6a92d72f
monjurulhaque64 <monjurulhoque64@gmail.com>
2023-06-10 23:29:10 +0600

    banner edit

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,40 +1,52 @@
 import React from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import banner1 from '../../../assets/banner/banner1.jpg';
 import banner2 from '../../../assets/banner/banner2.jpg';
 import banner3 from '../../../assets/banner/banner3.jpg';
 import './Banner.css';
+import { Navigation, Pagination } from "swiper";
 
 const Banner = () => {
-  return (
-    <Carousel>
-      <div className="banner-slide relative">
-        <img src={banner1} className="banner-image" alt="Banner 1" />
-        <div className="banner-content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-black">
-          <h2 className="text-3xl title font-bold mb-4 text-black">MindScape Summer Camp: Explore, Discover, Create!</h2>
-          <p className="text-lg hidden-on-mobile mb-6">Unleash your child's creativity and curiosity at MindScape Summer Camp - where unforgettable adventures await!</p>
-          <button className="btn btn-info">See All Classes</button>
-        </div>
-      </div>
-      <div className="banner-slide relative">
-        <img src={banner2} className="banner-image" alt="Banner 2" />
-        <div className="banner-content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-          <h2 className="text-3xl title font-bold mb-4">Unleash Your Imagination at MindScape Summer Camp</h2>
-          <p className="text-lg hidden-on-mobile mb-6">Join us for an enriching summer experience at MindScape Summer Camp - where learning and fun collide!</p>
-          <button className="btn btn-info">See All Classes</button>
-        </div>
-      </div>
-      <div className="banner-slide relative">
-        <img src={banner3} className="banner-image" alt="Banner 3" />
-        <div className="banner-content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-          <h2 className="text-3xl title font-bold mb-4">MindScape Summer Adventures: Ignite Your Curiosity</h2>
-          <p className="text-lg hidden-on-mobile  mb-6">Discover a world of excitement and friendship at MindScape Summer Camp - the perfect summer destination!</p>
-          <button className="btn btn-info">See All Classes</button>
-        </div>
-      </div>
-    </Carousel>
-  );
+    return (
+        <Swiper>
+            <SwiperSlide>
+                <div className="banner-slide relative">
+                    <img src={banner1} className="banner-image" alt="Banner 1" />
+                    <div className="banner-content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-black">
+                        <h2 className="text-3xl title font-bold mb-4 text-black">MindScape Summer Camp: Explore, Discover, Create!</h2>
+                        <p className="text-lg hidden-on-mobile mb-6">Unleash your child's creativity and curiosity at MindScape Summer Camp - where unforgettable adventures await!</p>
+                        <button className="btn btn-info">See All Classes</button>
+                    </div>
+                </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <div className="banner-slide relative">
+                    <img src={banner2} className="banner-image" alt="Banner 2" />
+                    <div className="banner-content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+                        <h2 className="text-3xl title font-bold mb-4">Unleash Your Imagination at MindScape Summer Camp</h2>
+                        <p className="text-lg hidden-on-mobile mb-6">Join us for an enriching summer experience at MindScape Summer Camp - where learning and fun collide!</p>
+                        <button className="btn btn-info">See All Classes</button>
+                    </div>
+                </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <div className="banner-slide relative">
+                    <img src={banner3} className="banner-image" alt="Banner 3" />
+                    <div className="banner-content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+                        <h2 className="text-3xl title font-bold mb-4">MindScape Summer Adventures: Ignite Your Curiosity</h2>
+                        <p className="text-lg hidden-on-mobile  mb-6">Discover a world of excitement and friendship at MindScape Summer Camp - the perfect summer destination!</p>
+                        <button className="btn btn-info">See All Classes</button>
+                    </div>
+                </div>
+            </SwiperSlide>
+
+        </Swiper>
+    );
 };
 
 export default Banner;
--- src/Pages/Home/PropulerClass/PopulerCard/PopuerlCard.jsx
+++ src/Pages/Home/PropulerClass/PopulerCard/PopuerlCard.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 
 const PopuerlCard = ({ classData }) => {
-  const { name } = classData;
+    const { name , instructorName, availableSeats, price, image} = classData;
   return (
       <div className="card w-[350px] lg:w-96 bg-base-100 shadow-xl">
         <figure>
-          <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+          <img src={image} alt={name} />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">
-            {name}
-            <div className="badge badge-secondary text-white bg-blue-500">POPULER</div>
-          </h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">Fashion</div>
-            <div className="badge badge-outline">Products</div>
-          </div>
+        <h2 className="card-title font-extrabold text-3xl uppercase text-blue-400 justify-center">{name}</h2>
+                    <p className='text-lg'> Instructor: {instructorName}</p>
+                    <h3 className='font-semibold'>Available seats: {availableSeats}</h3>
+                    <h3 className='font-semibold'>Price: ${price}</h3>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-outline btn-info">Enroll</button>
+                    </div>
         </div>
       </div>
   );
